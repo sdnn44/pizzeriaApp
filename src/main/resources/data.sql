6c7507983a63bb38fae841c5e2239a7b722d7c29
sdnn44 <slidennnq@gmail.com>
2022-06-13 17:17:43 +0200

    Added data to db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO sizes(name,diameter) VALUES ('Mała',32);
+INSERT INTO sizes(name,diameter) VALUES ('Średnia',37);
 INSERT INTO sizes(name,diameter) VALUES ('Duża',42);
-INSERT INTO sizes(name,diameter) VALUES ('Średnia',52);
 
 INSERT INTO ingredients(name,is_vegetarian,allergens)
     VALUES ('Sos pomidorowy',true,null);
@@ -10,9 +10,32 @@ INSERT INTO ingredients(name,is_vegetarian,allergens)
 INSERT INTO products(name,size_id,price)
     VALUES ('Margherita',1,20.00);
 INSERT INTO products(name,size_id,price)
+    VALUES ('Parma',1,20.00);
+INSERT INTO products(name,size_id,price)
     VALUES ('Margherita',2,25.00);
+INSERT INTO products(name, size_id, price)
+    VALUES ('Zuccini', 3, 19.00);
+INSERT INTO products(name, size_id, price)
+    VALUES ('Capriciosa', 3, 19.00);
 
 INSERT INTO products_ingredients(product_id, ingredient_id)
     VALUES (1,1);
 INSERT INTO products_ingredients(product_id, ingredient_id)
     VALUES (1,2);
+INSERT INTO products_ingredients(product_id, ingredient_id)
+    VALUES (2,1);
+INSERT INTO products_ingredients(product_id, ingredient_id)
+    VALUES (2,2);
+INSERT INTO products_ingredients(product_id, ingredient_id)
+VALUES (3,1);
+INSERT INTO products_ingredients(product_id, ingredient_id)
+VALUES (3,2);
+
+INSERT INTO products_ingredients(product_id, ingredient_id)
+VALUES (4,1);
+INSERT INTO products_ingredients(product_id, ingredient_id)
+VALUES (4,2);
+INSERT INTO products_ingredients(product_id, ingredient_id)
+VALUES (5,1);
+INSERT INTO products_ingredients(product_id, ingredient_id)
+VALUES (5,2);
